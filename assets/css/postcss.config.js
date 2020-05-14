116d356dc0489675226bd334178428a42b9236ec
Sam Killermann <sK@hues.xyz>
2020-05-14 01:20:23 -0500

    updated to latest hugo and that seems to have fixed the problem?

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,28 +1,36 @@
-class TailwindExtractor {
-	static extract(content) {
-		return content.match(/[A-z0-9-:\/]+/g)
-	}
-}
+const themeDir = __dirname + '/../../';
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        themeDir + 'layouts/**/*.html',
+        themeDir + 'exampleSite/content/**/*.html',
+        'layouts/**/*.html',
+        'content/**/*.html',
+    ],
+
+    // This is the function used to extract class names from your templates
+    defaultExtractor: content => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+        return broadMatches.concat(innerMatches)
+    }
+})
 
 module.exports = {    
-  plugins: [        
-    require('postcss-import')({
-      path: ["assets/css"],
-    }), 
-    require('tailwindcss')('./assets/css/tailwind.config.js'),    
-    require('@fullhuman/postcss-purgecss')({
-      content: ['layouts/**/*.html'],
-      extractors: [
-      {
-        extractor: TailwindExtractor,
-        extensions: ['html']
-      }], 
-      fontFace: false,
-      whitelist: ['class1', 'class2']
-    }),    
-    require('autoprefixer')({
-      grid: true,
-      overrideBrowserslist: ['>1%']
-    }),    
-  ]
+    plugins: [        
+        require('postcss-import')({
+            path: [themeDir]
+            }), 
+        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
+        require('autoprefixer')({
+            path: [themeDir]
+        }),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
+    ]
 }
